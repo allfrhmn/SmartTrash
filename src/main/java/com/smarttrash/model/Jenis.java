574e9bc988d93bd9985df92dfb281994d82dd65b
allfrhmn <allfiandirhmn@gmail.com>
2024-01-01 20:36:51 +0700

    Perbaiki model jenis

---
+++ src/main/java/com/smarttrash/model/Jenis.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smarttrash.model;
+
+/**
+ *
+ * @author Deni P
+ */
+
+// membuat class Jenis yang berfungsi untuk mengatur data yang ada di dalam database
+public class Jenis {
+    // deklarasi variabel
+    private int idJenis;
+    private String namaJenis;
+    private Kategori kategori;
+
+    /* 
+        * << Kumpulan Getter dan Setter >>
+        * membuat getter dan setter untuk mengambil dan mengisi data
+     */
+
+    public int getIdJenis() {
+        return idJenis;
+    }
+
+    public void setIdJenis(int idJenis) {
+        this.idJenis = idJenis;
+    }
+
+    public String getNamaJenis() {
+        return namaJenis;
+    }
+
+    public void setNamaJenis(String namaJenis){
+        this.namaJenis = namaJenis;
+    }
+
+    public Kategori getKategori(){
+        return kategori;
+    }
+
+    public void setKategori(Kategori kategori){
+        this.kategori = kategori;
+    }
+}
