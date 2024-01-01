990e67514b53c310218f54c89e9588a313f53dab
allfrhmn <allfiandirhmn@gmail.com>
2024-01-01 07:36:27 +0700

    Perbaiki query dari database SmartTrash

--- src/main/java/com/smarttrash/dao/PoinDao.java
+++ src/main/java/com/smarttrash/dao/PoinDao.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+// package dao ini berfungsi untuk mengatur data yang ada di dalam database
 package com.smarttrash.dao;
 
 /**
